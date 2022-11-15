54af974435521b3dd91fa46308b2619e7d1234e0
alya <alya-892@yandex.ru>
2022-11-15 18:12:20 +0300

    Задача2

--- EX002/Program.cs
+++ EX002/Program.cs
@@ -1,5 +1,10 @@
 ﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов. 
-
+// Например, задан массив: 
+// 1 4 7 2 
+// 5 9 2 3 
+// 8 4 2 4 
+// 5 2 6 7 
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка 
 // 1. получить число(сток/столбцов) 
 int Numb(string text) 
 { 
@@ -27,31 +32,34 @@ void Print(int[,] ar)
     for (int i = 0; i < ar.GetLength(0); i++, Console.WriteLine(" ")) 
     { 
         for (int j = 0; j < ar.GetLength(1); j++) 
-        { Console.Write(ar[i,j] + "\t"); } 
+        { Console.Write(ar[i, j] + "\t"); } 
     } 
-} 
- // 5. находить строку с наименьшей суммой элементов 
-void FindMinSunRowArray(int[,]array) 
-{ 
-    string text="Строка с наименьшей суммой элементов - это строка: № "; 
-    int sumMin=1; 
-     for (int i = 0; i < array.GetLength(0); i++, Console.WriteLine(" ")) 
-    { 
-        for (int j = 0; j < array.GetLength(1); j++) 
-        { int sum=0; 
-        sum+=array[i,j]; 
-        if(sum<sumMin) sumMin=j+1; 
-        } 
-    } 
-    Console.WriteLine(text+sumMin); 
 } 
  
+// 5. находить строку с наименьшей суммой элементов  
+void FindMinSunRowArray(int[,]array)  
+{  
+    string text="Строка с наименьшей суммой элементов - это строка: № ";  
+    int sumMin=0;  
+     for (int i = 0; i < array.GetLength(0); i++, Console.WriteLine(" "))  
+    {  
+        for (int j = 0; j < array.GetLength(1); j++)  
+        { int sum=0;  
+       sumMin=sumMin+array[1,j];
+        sum=array[i,j]+sum;  
+
+        if(sum<sumMin) sumMin=i;  
+        }  
+    }  
+    Console.WriteLine(text+sumMin);  
+}   
+
  
 Console.Clear(); 
-int Row=Numb("количество строк"); 
-int Column=Numb("количество столбщов"); 
-int[,]array=CreateArray(Row,Column); 
+int Row = Numb("количество строк"); 
+int Column = Numb("количество столбщов"); 
+int[,] array = CreateArray(Row, Column); 
 Console.WriteLine(); 
-FillArray(array,0,15); 
+FillArray(array, 0, 25); 
 Print(array); 
 FindMinSunRowArray(array);
